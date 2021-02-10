de9721c6fcb3a017a53707194e1efb5433a65914
Penko Gadzhev <penko032@gmail.com>
2021-02-10 18:38:39 +0200

    no time for final sprint

--- src/components/location/LocationCard.js
+++ src/components/location/LocationCard.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React,{useState} from 'react';
+import { makeStyles,withStyles } from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core';
+import CharCard from '../episodes/CharCard';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import MuiDialogActions from '@material-ui/core/DialogActions';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 const useStyles = makeStyles({
   root: {
@@ -15,10 +24,58 @@ const useStyles = makeStyles({
   },
 });
 
+
+
+
+// MODAL
+
+const DialogTitle = withStyles()((props) => {
+  const { children, onClose, ...other } = props;
+  const classes = useStyles();
+
+  return (
+    <MuiDialogTitle  disableTypography MuiDialog-paper {...other}>
+      <Typography variant="h6">{children}</Typography>
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          className={classes.closeButton}
+          onClick={onClose}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
+});
+
+const DialogContent = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(8),
+  },
+}))(MuiDialogContent);
+
+const DialogActions = withStyles((theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(1),
+  },
+}))(MuiDialogActions);
+
+
+
+
 const LocationCard = (props) => {
   const classes = useStyles();
 
-  
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <Card className={classes.root}>
@@ -36,6 +93,48 @@ const LocationCard = (props) => {
           <Typography variant="body2" color="#fff" component="p">
             Created: {props.created}
           </Typography>
+          <Button
+          className={classes.btn}
+          variant="contained"
+          color="primary"
+          onClick={handleClickOpen}
+          
+        >
+          <span className={classes.textButton}>Show Details</span>
+        </Button>
+        <Dialog
+          onClose={handleClose}
+          aria-labelledby="customized-dialog-title"
+          open={open}
+          maxWidth="xl"
+          className={classes.dialog}
+          disableScrollLock={ true }
+        >
+          <div className={classes.detailsHeading}>
+            <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+              <Typography align='center' variant="h4" component="h2">
+                Detail View
+              </Typography>
+              {/* <Typography align='center' variant="h6" component="h2">
+                Date: {props.air_date}
+              </Typography> */}
+            </DialogTitle>
+          </div>
+          {/* <DialogContent dividers>
+            <Typography gutterBottom variant="h5" component="h2">
+              <Grid container spacing={8}>
+                {props.location.map((char, index) => {
+                  return (
+                    <Grid key={index} item md={4}>
+                      <CharCard key={index} props={char} />
+                    </Grid>
+                  );
+                })}
+              </Grid>
+            </Typography>
+          </DialogContent> */}
+          <DialogActions></DialogActions>
+        </Dialog>
         </CardContent>
       </CardActionArea>
    
